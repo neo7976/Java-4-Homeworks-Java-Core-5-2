fde3b66a443ee65e202abd927b2f43ac411be024
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-03 18:29:22 +0300

    Написание кода

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+
+    public static void parseXML() throws ParserConfigurationException, IOException, SAXException {
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document document = builder.parse("src/main/resources/data.xml");
     }
 }
