5730a04a0b3c58f52ccdbd1221d57cbd1eb30c18
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-04 17:58:14 +0300

    Пустая запись Employee

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -9,11 +9,26 @@ public class Employee {
         // Пустой конструктор
     }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
     public Employee(long id, String firstName, String lastName, String country, int age) {
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.country = country;
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", country='" + country + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,46 +12,60 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-//        List<Employee> list = parseXML("src/main/resources/data.xml");
-        parseXML("src/main/resources/data.xml");
+        List<Employee> list = parseXML("src/main/resources/data.xml");
+        System.out.println(list.size());
+        for (Employee employee : list) {
+            System.out.println(employee);
+        }
     }
 
 
-    public static void parseXML(String fileWay) throws ParserConfigurationException, IOException, SAXException {
+    public static List<Employee> parseXML(String fileWay) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document document = builder.parse(new File(fileWay));
 
         Node root = document.getDocumentElement();
         System.out.println("Корневой узел - " + root.getNodeName());
-        read(root);
+        return read(root);
+
     }
 
-    private static void read(Node node) {
+    private static List<Employee> read(Node node) {
         List<Employee> employees = new ArrayList<>();
         NodeList nodeList = node.getChildNodes();
         for (int i = 0; i < nodeList.getLength(); i++) {
             Node node_ = nodeList.item(i);
             if (Node.ELEMENT_NODE == node_.getNodeType()) {
-                extracted(node_, "id");
-                extracted(node_, "firstName");
-                extracted(node_, "lastName");
-                extracted(node_, "country");
-                extracted(node_, "age");
+                Employee employee = new Employee();
+
+                if (node_.getNodeName().equals("id")) {
+                    System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
+                    employee.id = Long.parseLong(node_.getTextContent());
+                } else if (node_.getNodeName().equals("firstName")) {
+                    System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
+                    employee.firstName = node_.getTextContent();
+
+                } else if (node_.getNodeName().equals("lastName")) {
+                    System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
+                    employee.lastName = node_.getTextContent();
+
+                } else if (node_.getNodeName().equals("country")) {
+                    System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
+                    employee.country = node_.getTextContent();
+
+                } else if (node_.getNodeName().equals("age")) {
+                    System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
+                    employee.age = Integer.parseInt(node_.getTextContent());
+                }
+                employees.add(employee);
             }
+            read(node_);
 //             List<String> staff = new ArrayList<>();
 //             staff.add(node_.getTextContent());
 //                System.out.println(staff);
-
-
-            read(node_);
         }
-    }
-
-    private static void extracted(Node node_, String nameNode) {
-        if (node_.getNodeName().equals(nameNode)) {
-            System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
-        }
+        return employees;
     }
 
 }
