8aca59cc76c4ca6f29fabcbdefafb89d2604993c
Dmitrii Sobin <neo7976@yandex.ru>
2022-08-17 19:39:30 +0300

    Получилось вызвать значения сотрудников

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,62 +12,42 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException {
-        List<Employee> list = parseXML("src/main/resources/data.xml");
-        System.out.println(list.size());
-        for (Employee employee : list) {
-            System.out.println(employee);
-        }
+//        List<Employee> list = parseXML("src/main/resources/data.xml");
+        parseXML("src/main/resources/data.xml");
+
+//        System.out.println(list.size());
+//        for (Employee employee : list) {
+//            System.out.println(employee);
+//        }
     }
 
 
-    public static List<Employee> parseXML(String fileWay) throws ParserConfigurationException, IOException, SAXException {
+    public static void parseXML(String fileWay) throws ParserConfigurationException, IOException, SAXException {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         DocumentBuilder builder = factory.newDocumentBuilder();
         Document document = builder.parse(new File(fileWay));
 
         Node root = document.getDocumentElement();
         System.out.println("Корневой узел - " + root.getNodeName());
-        return read(root);
-
-    }
-
-    private static List<Employee> read(Node node) {
-        List<Employee> employees = new ArrayList<>();
-        NodeList nodeList = node.getChildNodes();
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node node_ = nodeList.item(i);
-            if (Node.ELEMENT_NODE == node_.getNodeType()) {
-                Employee employee = new Employee();
-
-                if (node_.getNodeName().equals("id")) {
-                    System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
-                    employee.id = Long.parseLong(node_.getTextContent());
-                } else if (node_.getNodeName().equals("firstName")) {
-                    System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
-                    employee.firstName = node_.getTextContent();
-
-                } else if (node_.getNodeName().equals("lastName")) {
-                    System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
-                    employee.lastName = node_.getTextContent();
-
-                } else if (node_.getNodeName().equals("country")) {
-                    System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
-                    employee.country = node_.getTextContent();
-
-                } else if (node_.getNodeName().equals("age")) {
-                    System.out.println(node_.getNodeName() + " - " + node_.getTextContent());
-                    employee.age = Integer.parseInt(node_.getTextContent());
+//        List<Employee> employeesList = new ArrayList<>();
+        //Просматриваем все элементы корневого - т.е. сотрудников
+        NodeList employees = root.getChildNodes();
+        for (int i = 0; i < employees.getLength(); i++) {
+            Node employee = employees.item(i);
+            //Если нода не текст, то это книга - заходим внутрь
+            if (employee.getNodeType()!=Node.TEXT_NODE){
+                NodeList employeeStaffs = employee.getChildNodes();
+                for (int j = 0; j < employeeStaffs.getLength(); j++) {
+                    Node employeeStaff = employeeStaffs.item(j);
+                    //Если нода не текст, то это один из параметров
+                    if(employeeStaff.getNodeType()!=Node.TEXT_NODE) {
+                        System.out.println(employeeStaff.getNodeName() + ":" + employeeStaff.getChildNodes().item(0).getTextContent());
+                    }
                 }
-                employees.add(employee);
+                System.out.println("=========>>>>>>>>>");
             }
-            read(node_);
-//             List<String> staff = new ArrayList<>();
-//             staff.add(node_.getTextContent());
-//                System.out.println(staff);
         }
-        return employees;
     }
-
 }
 
 
